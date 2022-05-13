c65affef776a290c2dd95e34721581d37abf3902
franlue <francoelucas@gmail.com>
2022-05-13 11:54:44 -0300

    Practicando JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-alert('¡IMPORTANTE! ACTUALICE SU NAVEGADOR!')
+let entrada = prompt("Ingresar un número");
+let iva = 1.21;
+let salida = "El valor total más IVA es igual a " + entrada * iva;
+alert(salida);
