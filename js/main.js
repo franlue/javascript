693a8f28c7513e7d492fc4fc40627531e06bf21e
Franco Lucas <francoelucas@gmail.com>
2022-05-19 21:14:02 -0300

    Terminando primer desafío complementario

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let numero = prompt("Ingrese el código 234560");
 
 while(numero != 234560){
-    alert("Código incorrecto");
+    if (numero == ""){
+        alert("No ingresaste ningún código")
+    }
+    alert("Incorrecto: NO puedes ingresar al sitio");
     numero=prompt("Intente nuevamente con el código 234560 ");
 }
