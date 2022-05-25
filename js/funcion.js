23829fdb8454f31112d62c2c5f923bb6a8a38cdc
Franco Lucas <francoelucas@gmail.com>
2022-05-24 23:54:51 -0300

    Completando primer desafío

---
+++ js/funcion.js
@@ -0,0 +1,15 @@
+let resultado = 0;
+
+function sumar(primerNumero, segundoNumero) {
+
+    resultado = primerNumero + segundoNumero
+
+}
+
+function mostrar (mensaje) {
+
+    console.log(mensaje)
+
+}
+sumar(8, 9);
+mostrar(resultado);
--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-let numero = prompt("Ingrese el código 234560");
+let numero = prompt("Bienvenido a Reiyspak, ingrese el número de la opción deseada\n Catálogo de productos [1] \n Comprar [2]");
 
-while(numero != 234560){
+while ((numero !=1)&&(numero != 2)){
     if (numero == ""){
-        alert("No ingresaste ningún código")
+        alert("No ingresaste ninguna opción")
     }
-    alert("Incorrecto: NO puedes ingresar al sitio");
-    numero=prompt("Intente nuevamente con el código 234560 ");
+    alert("Ingresa una opción correcta")
+    numero = prompt("\n Catálogo de productos [1] \n Comprar [2]")
 }
+
+if(numero == ""){
+
+    alert("No ingresaste ningún código")
+    numero = prompt ("Intente nuevamente")
+
+}else if(numero == "1") {
+
+    alert("Catálogo \n RIG DE MINERIA \n PLACA DE VIDEO \n FUENTE MODULAR")
+
+} else if (numero == "2") {
+
+    alert("Comprar")
+
+} else {
+
+    alert("Ingresa una opción correcta")
+    numero = prompt ("\n Catálogo de productos [1] \n Comprar [2]")
+
+} 
