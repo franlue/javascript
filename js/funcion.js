490a5425d3c70de7f4da2a02613bb8dab008e0bf
franlue <francoelucas@gmail.com>
2022-05-26 12:18:05 -0300

    Mejorando simulador, agregando funciones

--- js/funcion.js
+++ js/funcion.js
@@ -1,15 +1,23 @@
-let resultado = 0;
+// let resultado = 0;
 
-function sumar(primerNumero, segundoNumero) {
+// function sumar(primerNumero, segundoNumero) {
 
-    resultado = primerNumero + segundoNumero
+//     resultado = primerNumero + segundoNumero
 
+// }
+
+// function mostrar (mensaje) {
+
+//     console.log(mensaje)
+
+// }
+// sumar(8, 9);
+// mostrar(resultado);
+
+function saludando(){
+    alert("Hola usuario");
+    console.log("Se ejecuta la función");
+    prompt("¿Te gustó la función? \n ¿Si? \n ¿No?")
 }
 
-function mostrar (mensaje) {
-
-    console.log(mensaje)
-
-}
-sumar(8, 9);
-mostrar(resultado);
+saludando();
--- js/main.js
+++ js/main.js
@@ -1,39 +1,57 @@
 let numero = prompt("Bienvenido a Reiyspak, ingrese el número de la opción deseada\n Catálogo de productos [1] \n Comprar [2]");
 let opcion = 0;
+let totalCompra = 0;
 
-while ((numero !=1 )&&(numero != 2)){
-    if (numero == ""){
-        alert("No ingresaste ninguna opción")
+function valor(){
+    while ((numero !=1 )&&(numero != 2)){
+        if (numero == ""){
+            alert("No ingresaste ninguna opción")
+        }
+        alert("Ingresa una opción correcta")
+        numero = prompt("\n Catálogo de productos [1] \n Comprar [2]")
     }
-    alert("Ingresa una opción correcta")
-    numero = prompt("\n Catálogo de productos [1] \n Comprar [2]")
+    
+    if(numero == 1) {
+    
+        alert("Catálogo \n RIG DE MINERIA : 30.000USD \n PLACA DE VIDEO : 4.000USD \n FUENTE MODULAR : 1.000USD \n BASE DE ACERO INOXIDABLE : 200USD");
+        redirigir();
+        numero = prompt("Si desea volver al  menú principal ingrese [3]")
+    
+    } else if (numero == 2) {
+    
+        alert("Comprar \n Elija el producto que quiera comprar \n RIG DE MINERIA [A] \n PLACA DE VIDEO [B] \n FUENTE MODULAR [C] \n BASE DE ACERO INOXIDABLE [D]")
+        opcion = prompt("Elija la opción")
+        if (opcion == "A"){
+            alert("Elegiste la opción A")
+        } else if (opcion == "B") {
+            alert("Elegiste la opción B")
+        } else if (opcion == "C") {
+            alert("Elegiste la opción C")
+        } else if (opcion == "D"){
+            alert("Elegiste la opción D")
+        } else {
+            alert("No elegiste ninguna opción")
+            numero = prompt("Para volver al menú principal elija 3")
+            redirigir();
+        }
+        valor();
+    
+    } else {
+    
+        alert("Ingresa una opción correcta")
+        valor();
+    
+    } 
 }
-
-if(numero == 1) {
-
-    alert("Catálogo \n RIG DE MINERIA : 30.000USD \n PLACA DE VIDEO : 4.000USD \n FUENTE MODULAR : 1.000USD \n BASE DE ACERO INOXIDABLE : 200USD")
-    numero = prompt("Si desea volver al  menú principal ingrese [3]")
-
-} else if (numero == 2) {
-
-    alert("Comprar \n Elija el producto que quiera comprar \n RIG DE MINERIA [A] \n PLACA DE VIDEO [B] \n FUENTE MODULAR [C] \n BASE DE ACERO INOXIDABLE [D]")
-    opcion = prompt("Elija la opción")
-    if (opcion == "A"){
-        alert("Elegiste la opción A")
+valor();
+function redirigir(){
+    while (numero == 3){
+        alert("Serás redirigido al menú principal")
+        valor ();
+        numero = prompt("\n Catálogo de productos [1] \n Comprar [2]")
+    
     }
-    opcion = prompt("Si desea volver al  menú principal ingrese [3]")
-
-} else {
-
-    alert("Ingresa una opción correcta")
-    numero = prompt("Si desea volver al  menú principal ingrese [3]")
-
-} 
-
-while (numero == 3){
-    alert("Serás redirigido al menú principal")
-    numero = prompt("\n Catálogo de productos [1] \n Comprar [2]")
-
 }
+redirigir();
 // No sé como hacer el código de manera que se pueda repetir el proceso.
 // Cuando el usuario ingrese "3" me gustaría que el código volviera a empezar.
